41a75ff4f0d3b4aef4d0b22281fcf48d0f3a73ff
Michael Delgado <Neochange@gmail.com>
2018-03-12 12:45:03 +0100

    Broadcast receiver detects when the device is connected to a specific SSID

--- app/src/main/java/com/example/michael/timecontrol/WifiChangeStateReceiver.java
+++ app/src/main/java/com/example/michael/timecontrol/WifiChangeStateReceiver.java
@@ -3,15 +3,33 @@ package com.example.michael.timecontrol;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 
 public class WifiChangeStateReceiver extends BroadcastReceiver {
+
+    private static boolean connected = true;
+    private static String currentSSID = "";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
 
-        Log.d("WifiChangeStateReceiver", "WIFI state changed");
+        WifiManager wifiManager = (WifiManager) context.getSystemService (Context.WIFI_SERVICE);
+        WifiInfo info = wifiManager.getConnectionInfo();
+        String ssid  = info.getSSID();
+
+        if( connected && ssid =="<unknown ssid>"){
+            currentSSID = "";
+            connected = false;
+            Log.d("WifiChangeStateReceiver", "Wifi disconnected");
+        }
+        else if (!connected && ssid !="<unknown ssid>") {
+            currentSSID = ssid;
+            connected = true;
+            Log.d("WifiChangeStateReceiver", "Wifi connected: " + ssid);
+        }
     }
 }
